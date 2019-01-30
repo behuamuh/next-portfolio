e9fd964df8f1dbae579e12c686218a281d261549
Ben <sven1980@list.ru>
2019-01-30 16:28:12 +0300

    add fetch in about

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,24 @@
 import Layout from "../components/Layout";
+import fetch from "isomorphic-unfetch";
 
-const About = () => (
-  <Layout title="About">
-    <p>A JS developer</p>
-    <img src="/static/jslogo.png" alt="jslogo" width="200px" />
-  </Layout>
-);
+const URL = "https://api.github.com/users/behuamuh";
+
+const About = props => {
+  const { user } = props;
+  return (
+    <Layout title="About">
+      <p>{user.name || "BeHuaMuH"}</p>
+      <img src={user.avatar_url} alt="behuamuh" width="200px" />
+    </Layout>
+  );
+};
+
+About.getInitialProps = async () => {
+  const data = await fetch(URL);
+  const json = await data.json();
+  return {
+    user: json
+  };
+};
 
 export default About;
